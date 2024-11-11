7c7a551355309f2744292d082bff0faab58b8426
Mohsporty <m.alassdi@gmail.com>
2024-11-11 20:31:22 +0300

    Initial Commit

---
+++ TKTodoApp/TKTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TKTodoAppApp.swift
+//  TKTodoApp
+//
+//  Created by Mohammad  on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TKTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
