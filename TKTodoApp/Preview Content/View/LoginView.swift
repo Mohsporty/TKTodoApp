430235353224b1ed158257553b4c56462b93b1ab
Mohsporty <m.alassdi@gmail.com>
2024-11-13 19:10:58 +0300

    new file for login informations

--- TKTodoApp/Preview Content/View/LoginView.swift
+++ TKTodoApp/Preview Content/View/LoginView.swift
@@ -1,15 +1,67 @@
-//
-//  LoginView.swift
-//  TKTodoApp
-//
-//  Created by Mohammad  on 12/11/2024.
-//
-
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isPasswordVisible: Bool = false
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("login")
+                .resizable()
+                .frame(height: 400)
+            
+            VStack {
+                Text("Login")
+                    .bold()
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 20)
+                
+                HStack{
+                    Image(systemName: "envelope")
+                    TextField("Email", text: $email)
+                }
+                .padding(.vertical, 6)
+                .background(Divider(), alignment: .bottom)
+                .padding(.bottom, 8)
+                
+                HStack {
+                    Image(systemName: "lock")
+                    
+                    if isPasswordVisible {
+                        TextField("Password", text: $password)
+                        
+                    } else {
+                        SecureField("Password", text: $password)
+                    }
+                    
+                  
+                    Button(action: {
+                        isPasswordVisible.toggle()
+                    }) {
+                        Image(systemName: isPasswordVisible ? "eye.slash" : "eye")
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.vertical, 6)
+                .background(Divider(), alignment: .bottom)
+                .padding(.bottom, 8)
+                
+ // actions button
+                Button(action: {}, label:{ Text("Login")
+                    .bold()
+                    .foregroundStyle(Color.black)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical,15)
+                    .background(Color.gray)
+                    .cornerRadius(8)
+                       }  )
+            }
+            .padding()
+        }
     }
 }
 
