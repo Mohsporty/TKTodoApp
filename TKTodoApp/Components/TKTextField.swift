70b379483ffa03110fcfd0d4d128ac1f77ef727b
Mohsporty <m.alassdi@gmail.com>
2024-11-14 23:38:52 +0300

    signup button and making esaly code for information and make profiles

---
+++ TKTodoApp/Components/TKTextField.swift
@@ -0,0 +1,18 @@
+//
+//  TKTextField.swift
+//  TKTodoApp
+//
+//  Created by Mohammad  on 14/11/2024.
+//
+
+import SwiftUI
+
+struct TKTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TKTextField()
+}
