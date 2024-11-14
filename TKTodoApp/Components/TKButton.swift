1529c2fb0b0c21cda205903ba736d40f99102c21
Mohsporty <m.alassdi@gmail.com>
2024-11-14 18:11:51 +0300

    build 3 button for signin in first page

---
+++ TKTodoApp/Components/TKButton.swift
@@ -0,0 +1,49 @@
+//
+//  TKVutton.swift
+//  TKTodoApp
+//
+//  Created by Mohammad  on 14/11/2024.
+//
+
+import SwiftUI
+
+struct TKVutton: View {
+    
+    
+    var label: String
+    var iconName: String?
+    var iconImage: Image?
+    var action: () -> Void
+    var body: some View {
+        
+        
+        Button(action: action, label:{
+            HStack {
+                if let iconImage = iconImage {
+                   iconImage
+                        .resizable()
+                        .frame(width: 20, height: 20)
+        } else if let iconName = iconName {
+                    Image(systemName: iconName)
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                }
+                
+                
+                Text(label)
+                    .bold()
+                    .foregroundStyle(Color.black)
+                    
+            }
+               }  )
+        .frame(maxWidth: .infinity)
+        .padding(.vertical,15)
+        .background(Color.white)
+        .cornerRadius(8)
+        .shadow(color: .black, radius: 4, x: 0, y: 2)
+    }
+}
+
+#Preview {
+    
+}
---
+++ TKTodoApp/Preview Content/View/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  TKTodoApp
+//
+//  Created by Mohammad  on 14/11/2024.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}
