c9082457d2f6e97283ff7ae2e8fc9f21cee88ead
Mohsporty <m.alassdi@gmail.com>
2024-11-19 00:55:54 +0300

    clear some code and

---
+++ TKTodoApp/Extensions /ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  TKTodoApp
+//
+//  Created by Mohammad  on 19/11/2024.
+//
+
+import SwiftUI
+
+struct ColorExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorExtension()
+}
